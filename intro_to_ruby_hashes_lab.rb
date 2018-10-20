e106e6620b136d63e36708bb08c4f06d589fe1f7
Kristen Robertson <kristen77@gmail.com>
2018-10-20 15:24:40 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,12 +3,12 @@ def new_hash
 end
 
 def actor
-  actor = {:name=>"Dwayne The Rock Johnson"}
+  actor = {:name => "Dwayne The Rock Johnson"}
 end
 
 def monopoly
 	monopoly = {
-    :railroads=>{}
+    :railroads => {}
 }
 end
 
